fca928adae44d90a1a72d98d933f6a4f3ca600ae
patilnandini1811 <70803146+patilnandini1811@users.noreply.github.com>
2023-09-21 16:26:34 +0200

    Mock Server
    Make all necessary files and setup

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,3 +1,86 @@
-export const LoginPage = () => {
-	return <div>LoginPage</div>;
+import { useState, useRef } from 'react';
+import styles from './LoginPage.module.css'
+
+
+
+export const LoginPage = () =>
+{
+	const [ username, setUsername ] = useState('');
+	const [ password, setPassword ] = useState('');
+	const [ error, setError ] = useState('');
+	const usernameRef = useRef(null);
+	const passwordRef = useRef(null);
+
+	const handleLogin = async() =>
+	{
+		if (!username || !password)
+		{
+			setError('Both fields are requried!');
+			return;
+		}
+		if (password.length <= 5)
+		{
+			setError("Password should be more than 5 characters!");
+			return;
+		}
+		try {
+      const response = await fetch('/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        setError('');
+        resetForm();
+        // You can handle JWT or other response data here if needed.
+      } else {
+        setError(data.error || "An error occurred.");
+      }
+    } catch (err) {
+      setError("An error occurred while logging in.");
+    }
+  }
+
+  const resetForm = () => {
+    setUsername('');
+    setPassword('');
+    usernameRef.current.focus();
+  }
+  
+
+	return (
+		<div>
+			<h1>Login Page</h1>
+
+			{error && <p style={{ color: 'red' }}>{error}</p>}
+
+			<input
+				ref={usernameRef}
+				value={username}
+				onChange={e => setUsername(e.target.value)}
+				placeholder="Username"
+			/>
+			<input
+				ref={passwordRef}
+				type="password"
+				value={password}
+				onChange={e => setPassword(e.target.value)}
+				placeholder="Password"
+			/>
+			<button className={styles.button} onClick={handleLogin}>Login</button>
+
+      
+		</div>
+	);
 };
+
+
+
+	
+
+export default LoginPage;
