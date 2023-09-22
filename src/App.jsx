055e05d1dab613bad69438161c1279bb20a82d87
SabanaSK <Sornkarn_su@hotmail.com>
2023-09-22 14:30:14 +0200

    Created BookmarkContext
    - Contain Bookmarks, addBookmark and removeBookmark

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { HomePage } from "./pages/HomePage";
 import { LoginPage } from "./pages/LoginPage";
 import { MoviePage } from "./pages/MoviePage/MoviePage";
 import BookmarkPage from "./pages/BookmarkPage";
+import { BookmarkProvider } from "./context/BookmarkContext";
+
 function App() {
 	return (
 		<div>
@@ -13,12 +15,14 @@ function App() {
 				<Link to="/Netflix/bookmark">Bookmark</Link>
 			</div>
 
+     <BookmarkProvider>
 			<Routes>
 				<Route path="/Netflix" element={<HomePage />} />
 				<Route path="/Netflix/login" element={<LoginPage />} />
 				<Route path="/Netflix/movie/:movieId" element={<MoviePage />} />
 				<Route path="/Netflix/bookmark" element={<BookmarkPage />} />
 			</Routes>
+     </BookmarkProvider>
 		</div>
 	);
 }
