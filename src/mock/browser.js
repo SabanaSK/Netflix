4e63de2c001a82318104dac6d90ab7dd68b2eaa8
patilnandini1811 <70803146+patilnandini1811@users.noreply.github.com>
2023-09-22 10:21:37 +0200

    Mock-server
    Setup Mock Server

--- src/mock/browser.js
+++ src/mock/browser.js
@@ -1,6 +1,16 @@
-// src/mocks/browser.js
-import { setupWorker } from 'msw'
-import { handlers } from './handlers'
+// // src/mocks/browser.js
+// import { setupWorker } from 'msw'
+// import { handlers } from './handlers'
 
-// This configures a Service Worker with the given request handlers.
-export const worker = setupWorker(...handlers)
+// // This configures a Service Worker with the given request handlers.
+// export const worker = setupWorker(...handlers)
+
+import { setupWorker } from 'msw';
+import { handlers } from './handlers';
+
+const worker = setupWorker(...handlers);
+worker.start({
+  onUnhandledRequest: 'bypass'
+});
+
+console.log("MSW started");
