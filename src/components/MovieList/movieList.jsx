ed260dc6ba6aed34b3df115b753f8eb71c3d1771
SabanaSK <Sornkarn_su@hotmail.com>
2023-09-22 12:01:56 +0200

    Correct movies.json path in MovieList
    -Update import path of movies.json

--- src/components/MovieList/movieList.jsx
+++ src/components/MovieList/movieList.jsx
@@ -1,4 +1,4 @@
-import moviesData from '../../../movies.json';
+import moviesData from '../MovieList/movieList';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
