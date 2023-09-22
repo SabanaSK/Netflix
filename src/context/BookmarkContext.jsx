1129b20613c0a3c56ee0a7f013df52ceb3a6175c
Kate <kate.nordqvist@gmail.com>
2023-09-22 15:10:42 +0200

    implemented mock server

--- src/context/BookmarkContext.jsx
+++ src/context/BookmarkContext.jsx
@@ -1,36 +1,38 @@
-import { createContext, useContext, useState } from 'react';
-import PropTypes from "prop-types"; 
+import { createContext, /* useContext, */ useState } from "react";
+import PropTypes from "prop-types";
 
 const BookmarkContext = createContext();
 
-export const useBookmarks = () => {
+/* export const useBookmarks = () => {
   return useContext(BookmarkContext);
-}
+} */
 
 export const BookmarkProvider = ({ children }) => {
-  const [bookmarks, setBookmarks] = useState([]);
+	const [bookmarks, setBookmarks] = useState([]);
 
-  const addBookmark = (movie) => {
-    setBookmarks(prevBookmarks => [...prevBookmarks, movie]);
-  };
+	const addBookmark = (movie) => {
+		setBookmarks((prevBookmarks) => [...prevBookmarks, movie]);
+	};
 
-  const removeBookmark = (movieId) => {
-    setBookmarks(prevBookmarks => prevBookmarks.filter(movie => movie.id !== movieId));
-  };
+	const removeBookmark = (movieId) => {
+		setBookmarks((prevBookmarks) =>
+			prevBookmarks.filter((movie) => movie.id !== movieId)
+		);
+	};
 
-  const value = {
-    bookmarks,
-    addBookmark,
-    removeBookmark
-  };
+	const value = {
+		bookmarks,
+		addBookmark,
+		removeBookmark,
+	};
 
-  return (
-    <BookmarkContext.Provider value={value}>
-      {children}
-    </BookmarkContext.Provider>
-  );
+	return (
+		<BookmarkContext.Provider value={value}>
+			{children}
+		</BookmarkContext.Provider>
+	);
 };
 
 BookmarkProvider.propTypes = {
-  children: PropTypes.node.isRequired,
+	children: PropTypes.node.isRequired,
 };
