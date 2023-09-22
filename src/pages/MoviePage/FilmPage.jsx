f3ca52c8b31e9d3b6e9b1ea6f07ef10b37897604
Kate <kate.nordqvist@gmail.com>
2023-09-22 11:47:49 +0200

    displays ifo about movie

---
+++ src/pages/MoviePage/FilmPage.jsx
@@ -0,0 +1,38 @@
+import moviesData from "../../../movies.json";
+import "./moviePage.css";
+
+export const FilmPage = () => {
+	/* const { filmName } = useParams() */
+	const filmName = "The Godfather";
+	const movieItem = moviesData.find((movie) => movie.title === filmName);
+
+	return (
+		<div>
+			<h2>{movieItem.title}</h2>
+			<div className="image-info-container">
+				<div className="image-container">
+					<img
+						src={movieItem.thumbnail}
+						alt={movieItem.title}
+						className="movie-img"
+					/>
+				</div>
+				<div className="info-container">
+					<p>
+						{movieItem.year},{" "}
+						<span className="rating">{movieItem.rating}</span>
+					</p>
+					<p>Cast:</p>
+					<ul className="actor-list">
+						{movieItem.actors.map((actor, idx) => (
+							<li key={idx}>{actor}</li>
+						))}
+					</ul>
+
+					<p>Genre: {movieItem.genre}</p>
+					<p>Synopsis: {movieItem.synopsis}</p>
+				</div>
+			</div>
+		</div>
+	);
+};
