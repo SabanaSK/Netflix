969c5fb0054d16f237ccf3f8e3b7a36f9b78ec22
Kate <kate.nordqvist@gmail.com>
2023-09-25 14:51:22 +0200

    categorys list added
    gets each genre from movies.json and renders a select-list

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,18 +2,19 @@ import { useState } from "react";
 import MovieList from "../components/MovieList/movieList";
 import SearchBar from "../components/Searchbar/Searchbar";
 import useFilterMovies from "../components/Filter/useFilterMovies";
-import moviesData from "../movies.json"
+import moviesData from "../movies.json";
+import CategoryList from "../components/CategoryList/CategoryList";
 export const HomePage = () => {
-  const [searchQuery, setSearchQuery] = useState('');
+	const [searchQuery, setSearchQuery] = useState("");
 
-	const filteredMovies = useFilterMovies(moviesData, searchQuery, );
+	const filteredMovies = useFilterMovies(moviesData, searchQuery);
 
-	return(
-		<div> 
-	<p>HomePage</p>
-	<SearchBar setSearchQuery={setSearchQuery} />
-	<MovieList movies={filteredMovies}/>
-	</div>
-	)
-	;
+	return (
+		<div>
+			<p>HomePage</p>
+			<CategoryList />
+			<SearchBar setSearchQuery={setSearchQuery} />
+			<MovieList movies={filteredMovies} />
+		</div>
+	);
 };
