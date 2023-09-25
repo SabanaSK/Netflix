19fbad4b87358257bf9755e4828ded2128319ae6
Kate <kate.nordqvist@gmail.com>
2023-09-25 12:38:24 +0200

    try logging in - not working
    tried doing a test for login, but don't know how to go forward

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -3,10 +3,10 @@ import { it, describe, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router";
 import { UserProvider } from "../context/UserContext";
+import userEvent from "@testing-library/user-event";
 
-describe("App.js", () => {
-	it("Check if the App render very well", () => {
-		//render our App properly
+describe("Login/logout features", () => {
+	it("Should show Login Page", () => {
 		render(
 			<UserProvider>
 				<MemoryRouter initialEntries={["/Netflix"]}>
@@ -16,5 +16,35 @@ describe("App.js", () => {
 		);
 
 		expect(screen.getByText("Login Page")).toBeInTheDocument();
+	});
+
+	it("Should show Home Page when logged in", async () => {
+		const user = userEvent.setup();
+
+		render(
+			<UserProvider>
+				<MemoryRouter initialEntries={["/Netflix"]}>
+					<App />
+				</MemoryRouter>
+			</UserProvider>
+		);
+		const usernameInput = screen.getByPlaceholderText("Username");
+		expect(usernameInput).toBeInTheDocument();
+
+		await user.type(usernameInput, "admin");
+
+		const passwordInput = screen.getByPlaceholderText("Password");
+		expect(passwordInput).toBeInTheDocument();
+
+		await user.type(passwordInput, "password123");
+
+		const loginButton = screen.getByRole("button", { name: "Login" });
+		expect(loginButton).toBeInTheDocument();
+
+		await user.click(loginButton);
+		screen.debug();
+		const homePageText = screen.findByText("HomePage");
+
+		expect(homePageText).toBeInTheDocument();
 	});
 });
