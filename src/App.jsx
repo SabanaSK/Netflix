3b86bca92cfa28d656737e630616798d864555f0
Kate <kate.nordqvist@gmail.com>
2023-09-25 16:08:17 +0200

    added route
    added route to Netflix/category/:categoryName

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { HomePage } from "./pages/HomePage";
 import { LoginPage } from "./pages/LoginPage/LoginPage";
 import { MoviePage } from "./pages/MoviePage/MoviePage";
 import BookmarkPage from "./pages/BookmarkPage";
+import CategoryPage from "./pages/CategoryPage/CategoryPage";
 import { BookmarkProvider } from "./context/BookmarkContext";
 import { useContext } from "react";
 import { UserContext } from "./context/UserContext";
@@ -23,25 +24,52 @@ function App() {
 					<Route
 						path="/Netflix/"
 						element={
-							cookies.user ? <HomePage /> : <Navigate to="/Netflix/login" />
+							cookies.user ? (
+								<HomePage />
+							) : (
+								<Navigate to="/Netflix/login" />
+							)
 						}
 					/>
 					<Route
 						path="/Netflix/movie/:movieId"
 						element={
-							cookies.user ? <MoviePage /> : <Navigate to="/Netflix/login" />
+							cookies.user ? (
+								<MoviePage />
+							) : (
+								<Navigate to="/Netflix/login" />
+							)
 						}
 					/>
 					<Route
 						path="/Netflix/bookmark"
 						element={
-							cookies.user ? <BookmarkPage /> : <Navigate to="/Netflix/login" />
+							cookies.user ? (
+								<BookmarkPage />
+							) : (
+								<Navigate to="/Netflix/login" />
+							)
 						}
 					/>
 					<Route
 						path="/Netflix/login"
 						element={
-							!cookies.user ? <LoginPage /> : <Navigate to="/Netflix/" />
+							!cookies.user ? (
+								<LoginPage />
+							) : (
+								<Navigate to="/Netflix/" />
+							)
+						}
+					/>
+
+					<Route
+						path="/Netflix/category/:categoryName"
+						element={
+							cookies.user ? (
+								<CategoryPage />
+							) : (
+								<Navigate to="/Netflix/login" />
+							)
 						}
 					/>
 				</Routes>
