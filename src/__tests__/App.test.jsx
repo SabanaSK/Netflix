b0f44163e7aa8627cfe05b634d65cb43288f1966
Kate <kate.nordqvist@gmail.com>
2023-09-25 09:22:50 +0200

    fixed failing test

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -2,16 +2,19 @@ import App from "../App";
 import { it, describe, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router";
+import { UserProvider } from "../context/UserContext";
 
 describe("App.js", () => {
 	it("Check if the App render very well", () => {
 		//render our App properly
 		render(
-			<MemoryRouter initialEntries={["/Netflix"]}>
-				<App />
-			</MemoryRouter>
+			<UserProvider>
+				<MemoryRouter initialEntries={["/Netflix"]}>
+					<App />
+				</MemoryRouter>
+			</UserProvider>
 		);
 
-		expect(screen.getByText("HomePage")).toBeInTheDocument();
+		expect(screen.getByText("Login Page")).toBeInTheDocument();
 	});
 });
