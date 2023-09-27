92805615e554fead0fc7b6055e15bf19d61e9e1b
Kate <kate.nordqvist@gmail.com>
2023-09-27 11:27:42 +0200

    integration test for login-logout
    - test if home page is shown when after logging in
    - test if login page is shown after logging out

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,26 +1,9 @@
 import App from "../App";
-import { it, describe, expect, beforeAll, afterAll } from "vitest";
+import { it, describe, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router";
 import { UserProvider } from "../context/UserContext";
 import userEvent from "@testing-library/user-event";
-import { setupServer } from "msw/node";
-import { rest } from "msw";
-
-const server = setupServer(
-	// Describe the requests to mock.
-	rest.post("http://localhost:5173/login", (_req, res, ctx) => {
-		return res(
-			ctx.status(200),
-			ctx.json({
-				user: "admin",
-			})
-		);
-	})
-);
-
-beforeAll(() => server.listen());
-afterAll(() => server.close());
 
 describe("Login/logout features", () => {
 	it("Should show Home Page when logged in", async () => {
@@ -34,17 +17,12 @@ describe("Login/logout features", () => {
 			</UserProvider>
 		);
 		const usernameInput = screen.getByPlaceholderText("Username");
-		expect(usernameInput).toBeInTheDocument();
-
 		await user.type(usernameInput, "admin");
 
 		const passwordInput = screen.getByPlaceholderText("Password");
-		expect(passwordInput).toBeInTheDocument();
-
 		await user.type(passwordInput, "password123");
 
 		const loginButton = screen.getByRole("button", { name: "Login" });
-		expect(loginButton).toBeInTheDocument();
 
 		await user.click(loginButton);
 		const homePageText = await screen.findByText("HomePage");
