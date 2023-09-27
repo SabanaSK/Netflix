8da0ac3bc8b793411447c2cf67a7a3a6a6231193
Kate <kate.nordqvist@gmail.com>
2023-09-27 20:18:19 +0200

    test for it to show a movie when bookmarks is not empty
    - sets a movie in localstorage
    - finds said movie by alt text

--- src/__tests__/BookmarkPage.test.jsx
+++ src/__tests__/BookmarkPage.test.jsx
@@ -1,12 +1,12 @@
-import { render, screen, within } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { it, describe, expect } from "vitest";
-import { MoviePage } from "../pages/MoviePage/MoviePage";
-import { MemoryRouter, Route, Routes } from "react-router";
 import { BookmarkProvider } from "../context/BookmarkContext";
 import BookmarkPage from "../pages/BookmarkPage";
+import moviesData from "../movies.json";
+import { MemoryRouter } from "react-router";
 
 describe("Renders correctly", () => {
-	it.only("shows 'no bookmarks saved yet'", () => {
+	it("shows 'no bookmarks saved yet'", () => {
 		render(
 			<BookmarkProvider>
 				<BookmarkPage />
@@ -16,5 +16,29 @@ describe("Renders correctly", () => {
 		const noBookmarksText = screen.getByText("No bookmarks saved yet!");
 
 		expect(noBookmarksText).toBeInTheDocument();
+	});
+
+	it("shows a movie image when movie is in bookmark", async () => {
+		/* WHY DO I NEED MEMORYROUTER WHEN USING LOCALSTORAGE??? */
+
+		const bookmarks = [moviesData[0]];
+
+		window.localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+
+		expect(true).toBe(true);
+
+		render(
+			<BookmarkProvider>
+				<MemoryRouter initialEntries={["/Netflix/bookmark"]}>
+					<BookmarkPage />
+				</MemoryRouter>
+			</BookmarkProvider>
+		);
+
+		const movieImage = screen.getByAltText("The Shawshank Redemption");
+		expect(movieImage).toBeInTheDocument();
+
+		//clear to avoid test contamination
+		window.localStorage.clear();
 	});
 });
