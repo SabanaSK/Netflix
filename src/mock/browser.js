220d7cabe2c57bbe77f4801802f7280d474b7251
Kate <kate.nordqvist@gmail.com>
2023-09-27 11:08:53 +0200

    Removed everything associated to mockserver in browser
    - uninstalled msw
    - removed msw workerDirectory from package.json
    - deleted mockserviceWorker.js
    -deleted browser.js, handlers.jsx, mockUsers.json from mock folder
    -deleted mock folder
    -moved mockUsers to src
    -removed import mock browser from main

--- src/mock/browser.js
+++
@@ -1,8 +0,0 @@
-// // This configures a Service Worker with the given request handlers.
-import { setupWorker } from "msw";
-import { handlers } from "./handlers";
-
-export const worker = setupWorker(...handlers);
-worker.start({
-	onUnhandledRequest: "bypass",
-});
