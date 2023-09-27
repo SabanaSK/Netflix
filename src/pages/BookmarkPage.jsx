9246fec92e3b68ab1450c695f06497b0965598b9
SabanaSK <Sornkarn_su@hotmail.com>
2023-09-27 10:10:18 +0200

    Add and display Bookmarks movie
    -Added Bookmark in movie.jsx
    -Display saved Bookmarks
    -Added text if there is no saved movies

--- src/pages/BookmarkPage.jsx
+++ src/pages/BookmarkPage.jsx
@@ -1,5 +1,24 @@
-export const BookmarkPage = () => {
-	return <div>BookmarkPage</div>;
-};
+import { useContext } from "react";
+import { BookmarkContext } from "../context/BookmarkContext";
+import Movie from "../components/Movie/Movie";
 
-export default BookmarkPage;
+const Bookmarks = () => {
+  const { bookmarks } = useContext(BookmarkContext);
+
+  if (bookmarks.length === 0) {
+    return <p>No bookmarks saved yet!</p>;
+  }
+
+  return (
+    <div>
+      <h2>Your Bookmarks</h2>
+      <div className="bookmarks-list">
+        {bookmarks.map(movie => (
+          <Movie key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Bookmarks;
