1d7aa4a522efc8db7c708b0ba2c39e1fbf043988
patilnandini1811 <70803146+patilnandini1811@users.noreply.github.com>
2023-09-28 12:18:11 +0200

    Enhanced Testing for MovieList Component
    In this update, we have refined the test suite for the MovieList component to ensure higher reliability and accuracy.
    Trending & Recommended Section Verification

--- src/__tests__/MoviesList.test.jsx
+++ src/__tests__/MoviesList.test.jsx
@@ -1,46 +1,47 @@
-import MovieList from "../components/MovieList/movieList";
+import { render, screen } from "@testing-library/react";
 import { it, describe, expect, beforeEach } from "vitest";
-import { render, screen, within } from "@testing-library/react";
-import { MemoryRouter } from 'react-router-dom';
+import MovieList from "../components/MovieList/movieList";
+import moviesData from "../movies.json";
+import { BookmarkProvider } from "../context/BookmarkContext";
+import { UserProvider } from "../context/UserContext";
+import { MemoryRouter } from "react-router";
 
 describe("MovieList", () => {
-    const mockMovies = [
-        {
-            id: 1,
-            thumbnail: "https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_QL75_UX380_CR0,4,380,562_.jpg",
-            title: "The Shawshank Redemption",
-            year: 1994,
-            rating: "PG",
-            isTrending: true,
-        },
-    ];
-    
     beforeEach(() => {
         render(
-            <MemoryRouter>
-                <MovieList movies={mockMovies} />
-            </MemoryRouter>
+            <UserProvider>
+                <BookmarkProvider>
+                    <MemoryRouter initialEntries={["/Netflix"]}>
+                        <MovieList movies={moviesData} />
+                    </MemoryRouter>
+                </BookmarkProvider>
+            </UserProvider>
         );
     });
 
     it("renders the Trending and Recommended sections", () => {
-        expect(screen.getByText("Trending")).toBeInTheDocument();
-        expect(screen.getByText("Recommended for You")).toBeInTheDocument();
+        expect(screen.getByRole("heading", { name: /trending/i })).toBeInTheDocument();
+        expect(screen.getByRole("heading", { name: /recommended for you/i })).toBeInTheDocument();
     });
 
-    it("renders movies in Trending section", async () => {
-        const movieElements = await screen.findByAltText("The Shawshank Redemption");
-        expect(movieElements.length).toBeGreaterThan(0);
+    it("renders movies in the Recommended section by image source", () => {
+        const godfatherImageSrc = "https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_QL75_UX380_CR0,4,380,562_.jpg";
+        const godfatherImgElement = document.querySelector(`img[src="${godfatherImageSrc}"]`);
+        expect(godfatherImgElement).toBeInTheDocument();
+
+        
     });
 
-    it("renders the details for the movie 'The Shawshank Redemption'", () => {
-        const movieElement = screen.getByText("The Shawshank Redemption").closest('.movie-item');
-        const { getByText } = within(movieElement);
+    it("renders movies in the Trending section by image source", () => {
+        // Filter out movies that are trending
+        const trendingMovieImages = moviesData
+            .filter(movie => movie.isTrending)
+            .map(movie => movie.thumbnail);
 
-        expect(getByText("The Shawshank Redemption")).toBeInTheDocument();
-        expect(getByText("Year: 1994")).toBeInTheDocument();
-        expect(getByText("Rating: PG")).toBeInTheDocument();
+        // For each trending movie by imges
+        trendingMovieImages.forEach(src => {
+            const movieImgElement = document.querySelector(`img[src="${src}"]`);
+            expect(movieImgElement).toBeInTheDocument();
+        });
     });
-
 });
-
