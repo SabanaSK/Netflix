8e0c3d0ff052471f7f119a020e06a76e9c619703
aylinataei <aylin.ataei@hotmail.com>
2023-09-28 11:16:11 +0200

    added genders movies in category

--- src/pages/CategoryPage/CategoryPage.jsx
+++ src/pages/CategoryPage/CategoryPage.jsx
@@ -1,54 +1,20 @@
 import { useParams } from "react-router";
 import movies from "../../movies.json";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import styles from "./CategoryPage.module.css";
+import Movie from "../../components/Movie/Movie";
 
 const CategoryPage = () => {
   const { categoryName } = useParams();
 
-  const displayedMovies = new Set();
-
-  const uniqueMovies = [];
-  movies.forEach((movie) => {
-    if (movie.genre.includes(categoryName) && !displayedMovies.has(movie.id)) {
-      displayedMovies.add(movie.id);
-      uniqueMovies.push(movie);
-    }
-  });
-
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 6,
-    slidesToScroll: 1,
-  };
+  const filteredMovies = movies.filter((movie) =>
+    movie.genre.includes(categoryName)
+  );
 
   return (
     <div>
-      <h2 className={styles.categoryTitle}>{categoryName}</h2>
-      <ul>
-        <Slider {...settings}>
-          {uniqueMovies.map((movie) => (
-            <li key={movie.id} className={styles["movie-thumbnail"]}>
-              {/* <h3>{movie.title}</h3> */}
-              <img
-                src={movie.thumbnail}
-                alt={movie.title}
-                onError={({ currentTarget }) => {
-                  currentTarget.onerror = null;
-                  currentTarget.src =
-                    "https://www.gpo.gov/images/default-source/leadership-1/davis.jpg";
-                }}
-              />
-              <p>Year: {movie.year}</p>
-              <p>Rating: {movie.rating}</p>
-            </li>
-          ))}
-        </Slider>
-      </ul>
+      <h2>{categoryName}</h2>
+      {filteredMovies.map((movie) => (
+        <Movie key={movie.id} movie={movie} />
+      ))}
     </div>
   );
 };
