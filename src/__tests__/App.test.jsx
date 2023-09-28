c9125de4c4d3735409e9987bb5d460455cdf519e
Kate <kate.nordqvist@gmail.com>
2023-09-28 17:35:19 +0200

    removeCookie function after each test
    - removeCookie function which clears cookies
    - call removeCookie after each test

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,11 +1,20 @@
 import App from "../App";
-import { it, describe, expect } from "vitest";
+import { it, describe, expect, afterEach } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router";
 import { UserProvider } from "../context/UserContext";
 import userEvent from "@testing-library/user-event";
 
+const removeCookie = (name) => {
+	document.cookie = `${name}=1; expires=1 Jan 1970 00:00:00 GMT;`;
+};
+
 describe("Login/logout features", () => {
+	//Removes cookies to not contsminate tests
+	afterEach(() => {
+		removeCookie("user");
+	});
+
 	it("Should show Home Page when logged in", async () => {
 		const user = userEvent.setup();
 
@@ -40,6 +49,15 @@ describe("Login/logout features", () => {
 				</MemoryRouter>
 			</UserProvider>
 		);
+
+		const usernameInput = screen.getByPlaceholderText("Username");
+		await user.type(usernameInput, "admin");
+
+		const passwordInput = screen.getByPlaceholderText("Password");
+		await user.type(passwordInput, "password123");
+
+		const loginButton = screen.getByRole("button", { name: "Login" });
+		await user.click(loginButton);
 
 		const logoutButton = screen.getByRole("button", { name: "Logout" });
 		expect(logoutButton).toBeInTheDocument();
