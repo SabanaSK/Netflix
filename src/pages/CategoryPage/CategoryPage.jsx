b37c5feb2dfe7ebb4a561638268e40ae88f8fa37
aylinataei <114648016+aylinataei@users.noreply.github.com>
2023-09-28 11:38:12 +0200

    Render category movies (#73)
    * get category names from useParams
    
    * added category movies
    
    * added renders movies in category
    
    * added no photo availble
    
    * deleted categoryPage.module.css
    
    ---------
    
    Co-authored-by: Kate <kate.nordqvist@gmail.com>

--- src/pages/CategoryPage/CategoryPage.jsx
+++ src/pages/CategoryPage/CategoryPage.jsx
@@ -1,8 +1,22 @@
 import { useParams } from "react-router";
+import movies from "../../movies.json";
+import Movie from "../../components/Movie/Movie";
 
 const CategoryPage = () => {
-	const { categoryName } = useParams();
-	return <div>{categoryName}</div>;
+  const { categoryName } = useParams();
+
+  const filteredMovies = movies.filter((movie) =>
+    movie.genre.includes(categoryName)
+  );
+
+  return (
+    <div>
+      <h2>{categoryName}</h2>
+      {filteredMovies.map((movie) => (
+        <Movie key={movie.id} movie={movie} />
+      ))}
+    </div>
+  );
 };
 
 export default CategoryPage;
--- src/pages/MoviePage/MoviePage.jsx
+++ src/pages/MoviePage/MoviePage.jsx
@@ -3,37 +3,41 @@ import moviesData from "../../movies.json";
 import "./moviePage.css";
 import BookmarkIcon from "../../components/BookmarkIcon/BookmarkIcon";
 export const MoviePage = () => {
-	const { movieId } = useParams();
-	const movieItem = moviesData.find((movie) => movie.id === Number(movieId));
+  const { movieId } = useParams();
+  const movieItem = moviesData.find((movie) => movie.id === Number(movieId));
 
-	return (
-		<div>
-			<BookmarkIcon movie={movieItem} />
-			<h2>{movieItem.title}</h2>
-			<div className="image-info-container">
-				<div className="image-container">
-					<img
-						src={movieItem.thumbnail}
-						alt={movieItem.title}
-						className="movie-img"
-					/>
-				</div>
-				<div className="info-container">
-					<p>
-						{movieItem.year},{" "}
-						<span className="rating">{movieItem.rating}</span>
-					</p>
-					<p>Cast:</p>
-					<ul className="actor-list">
-						{movieItem.actors.map((actor, idx) => (
-							<li key={idx}>{actor}</li>
-						))}
-					</ul>
+  return (
+    <div>
+      <BookmarkIcon movie={movieItem} />
+      <h2>{movieItem.title}</h2>
+      <div className="image-info-container">
+        <div className="image-container">
+          <img
+            src={movieItem.thumbnail}
+            alt={movieItem.title}
+            className="movie-img"
+            onError={({ currentTarget }) => {
+              currentTarget.onerror = null;
+              currentTarget.src =
+                "https://www.gpo.gov/images/default-source/leadership-1/davis.jpg";
+            }}
+          />
+        </div>
+        <div className="info-container">
+          <p>
+            {movieItem.year}, <span className="rating">{movieItem.rating}</span>
+          </p>
+          <p>Cast:</p>
+          <ul className="actor-list">
+            {movieItem.actors.map((actor, idx) => (
+              <li key={idx}>{actor}</li>
+            ))}
+          </ul>
 
-					<p>Genre: {movieItem.genre}</p>
-					<p>Synopsis: {movieItem.synopsis}</p>
-				</div>
-			</div>
-		</div>
-	);
+          <p>Genre: {movieItem.genre}</p>
+          <p>Synopsis: {movieItem.synopsis}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
