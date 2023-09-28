92f068a517a9ef460356d3b2afa6c12fee653c54
Kate <kate.nordqvist@gmail.com>
2023-09-28 11:10:45 +0200

    test removing bookmark on icon press

--- src/components/BookmarkIcon/BookmarkIcon.jsx
+++ src/components/BookmarkIcon/BookmarkIcon.jsx
@@ -1,28 +1,39 @@
 import { useContext } from "react";
 import PropTypes from "prop-types";
 import styles from "./BookmarkIcon.module.css";
-import { BookmarkContext } from "../../context/BookmarkContext";  
+import { BookmarkContext } from "../../context/BookmarkContext";
 
 const BookmarkIcon = ({ movie }) => {
-  const { bookmarks, addBookmark, removeBookmark } = useContext(BookmarkContext);
-  
-  const isBookmarked = bookmarks.some((bookmark) => bookmark.id === movie.id);
-  
-  const toggleBookmark = () => {
-    if (isBookmarked) {
-      removeBookmark(movie.id);
-    } else {
-      addBookmark(movie);
-    }
-  };
+	const { bookmarks, addBookmark, removeBookmark } =
+		useContext(BookmarkContext);
 
-  return <i onClick={toggleBookmark} className={`${styles["bookmark-icon"]} ${isBookmarked ? styles.bookmarked : ""}`}>★</i>
+	const isBookmarked = bookmarks.some((bookmark) => bookmark.id === movie.id);
+
+	const toggleBookmark = () => {
+		if (isBookmarked) {
+			removeBookmark(movie.id);
+		} else {
+			addBookmark(movie);
+		}
+	};
+
+	return (
+		<i
+			onClick={toggleBookmark}
+			className={`${styles["bookmark-icon"]} ${
+				isBookmarked ? styles.bookmarked : ""
+			}`}
+			data-testid="bookmark-icon"
+		>
+			★
+		</i>
+	);
 };
 
 BookmarkIcon.propTypes = {
-  movie: PropTypes.shape({
-    id: PropTypes.number.isRequired,
-  }).isRequired,
+	movie: PropTypes.shape({
+		id: PropTypes.number.isRequired,
+	}).isRequired,
 };
 
 export default BookmarkIcon;
