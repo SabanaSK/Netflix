aeddefc8a38db38ca5c28b01d2abb1943e1336e8
patilnandini1811 <70803146+patilnandini1811@users.noreply.github.com>
2023-09-29 14:05:56 +0200

    CategoryList unit test. (#86)
    * CategoryList unit test.
    
      - Renders without errors
      - Displays default option
      - Displays all genres

---
+++ src/__tests__/CategoryList.test.jsx
@@ -0,0 +1,40 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { describe,it,expect,beforeEach } from 'vitest';
+
+import CategoryList from '../components/CategoryList/CategoryList';
+import moviesData from '../movies.json';
+
+describe('CategoryList', () => {
+    beforeEach(() => {
+        render(
+            <MemoryRouter initialEntries={['/']}>
+                <CategoryList />
+            </MemoryRouter>
+        );
+    });
+//test 1
+    it('renders without errors', () => {
+        const selectElement = screen.getByRole('combobox', { options: { hidden: true } }); // Use 'listbox' for <select>
+        expect(selectElement).toBeInTheDocument();
+    });
+// test 2
+    it('displays default option', () => {
+        const defaultOption = screen.getByText(/category/i);
+        expect(defaultOption).toBeInTheDocument();
+    });
+//test 3
+    it('displays all genres', () => {
+        const genres = moviesData.reduce((acc, movie) => {
+            const genresForMovie = movie.genre.split(',').map(g => g.trim());
+            return [...new Set([...acc, ...genresForMovie])];
+        }, []);
+        genres.forEach(genre => {
+            const option = screen.getByText(new RegExp(genre, 'i'));
+            expect(option).toBeInTheDocument();
+        });
+    });
+
+});
+
+
