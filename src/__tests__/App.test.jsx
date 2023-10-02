52d0a961fe3926fbabe1bd86fbc182485caf552d
Kate <110845948+KateN87@users.noreply.github.com>
2023-10-02 11:33:05 +0200

    Test bookmark features (#94)
    - Add mockMovies.json
    - Click 3 bookmarks on homepage go to bookmark page
    - Expect three correct movie titles to be in the document

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,9 +1,15 @@
 import App from "../App";
-import { it, describe, expect, afterEach } from "vitest";
+import { it, describe, expect, afterEach, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router";
 import { UserProvider } from "../context/UserContext";
 import userEvent from "@testing-library/user-event";
+
+//get mock data from mockMovies.json
+vi.mock("../movies.json", async () => {
+	const actual = await vi.importActual("./mockMovies.json");
+	return actual;
+});
 
 const removeCookie = (name) => {
 	document.cookie = `${name}=1; expires=1 Jan 1970 00:00:00 GMT;`;
@@ -65,5 +71,49 @@ describe("Login/logout features", () => {
 		await user.click(logoutButton);
 		const loginPageText = await screen.findByText("Login Page");
 		expect(loginPageText).toBeInTheDocument();
+	});
+});
+
+describe("Bookmark features", () => {
+	it("shows 3 correct movies on bookmark page after 3 bookmark icons are clicked on home page", async () => {
+		const user = userEvent.setup();
+
+		render(
+			<UserProvider>
+				<MemoryRouter initialEntries={["/Netflix"]}>
+					<App />
+				</MemoryRouter>
+			</UserProvider>
+		);
+
+		const usernameInput = screen.getByPlaceholderText("Username");
+		await user.type(usernameInput, "admin");
+
+		const passwordInput = screen.getByPlaceholderText("Password");
+		await user.type(passwordInput, "password123");
+
+		const loginButton = screen.getByRole("button", { name: "Login" });
+		await user.click(loginButton);
+
+		const bookmarkIcons = screen.getAllByTestId("bookmark-icon");
+
+		await user.click(bookmarkIcons[5]);
+		await user.click(bookmarkIcons[3]);
+		await user.click(bookmarkIcons[2]);
+
+		const bookmarkLink = screen.getByRole("link", { name: "Bookmark" });
+		expect(bookmarkLink).toHaveAttribute("href", "/Netflix/bookmark");
+
+		await user.click(bookmarkLink);
+
+		const movieAltText1 = screen.getByAltText(
+			"Star Wars: Episode V - The Empire Strikes Back"
+		);
+		const movieAltText2 = screen.getByAltText("Casablanca");
+		const movieAltText3 = screen.getByAltText("Psycho");
+
+		expect(movieAltText1).toBeInTheDocument();
+		expect(movieAltText2).toBeInTheDocument();
+		expect(movieAltText3).toBeInTheDocument();
 	});
 });
