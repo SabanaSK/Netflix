8cc671d0648a15585ff73a0509146af2429b69ab
aylinataei <114648016+aylinataei@users.noreply.github.com>
2023-10-02 11:45:10 +0200

    show "no movie found" if searched movie doesnt exist (#96)

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,29 +1,34 @@
 import { useState } from "react";
-import MovieList from "../../components/MovieList/movieList"; 
+import MovieList from "../../components/MovieList/movieList";
 import SearchBar from "../../components/Searchbar/Searchbar";
 import CategoryList from "../../components/CategoryList/CategoryList";
 import Movie from "../../components/Movie/Movie";
-import useFilterMovies from "../../hooks/useFilterMovies";	
+import useFilterMovies from "../../hooks/useFilterMovies";
 
 export const HomePage = () => {
-	const [searchQuery, setSearchQuery] = useState("");
-	const filteredMovies = useFilterMovies({ searchQuery });
+  const [searchQuery, setSearchQuery] = useState("");
+  const filteredMovies = useFilterMovies({ searchQuery });
 
-	return (
-		<div>
-			<h2>HomePage</h2>
-			<CategoryList />
-			<SearchBar setSearchQuery={setSearchQuery}  />
-			{searchQuery ? (
-				<div>
-					<h2>Search Results</h2>
-					{filteredMovies.map((movie) => (
-						<Movie key={movie.id} movie={movie} />
-					))}
-				</div>
-			) : (
-				<MovieList />
-			)}
-		</div>
-	);
+  return (
+    <div>
+      <p>HomePage</p>
+      <CategoryList />
+      <SearchBar setSearchQuery={setSearchQuery} />
+
+      {searchQuery ? (
+        <div>
+          <h2>Search Results</h2>
+          {filteredMovies.length === 0 ? (
+            <p>No movie found.</p>
+          ) : (
+            filteredMovies.map((movie) => (
+              <Movie key={movie.id} movie={movie} />
+            ))
+          )}
+        </div>
+      ) : (
+        <MovieList />
+      )}
+    </div>
+  );
 };
