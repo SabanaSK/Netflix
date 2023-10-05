637889f0a086cf8031dbcca3d1da615af94b0f52
patilnandini1811 <70803146+patilnandini1811@users.noreply.github.com>
2023-10-05 14:07:35 +0200

    Fix code setting

--- src/__tests__/MoviesList.test.jsx
+++ src/__tests__/MoviesList.test.jsx
@@ -1,75 +1,70 @@
-import { render, screen,within } from "@testing-library/react";
-import { it, describe, expect, beforeEach, vi} from "vitest";
+import { render, screen, within } from "@testing-library/react";
+import { it, describe, expect, beforeEach, vi } from "vitest";
 import MovieList from "../components/MovieList/movieList";
-import moviesData from "./mockMovies.json"
+import moviesData from "./mockMovies.json";
 import { BookmarkProvider } from "../context/BookmarkContext";
 import { UserProvider } from "../context/UserContext";
 import { MemoryRouter } from "react-router";
 
 vi.mock("../movies.json", async () => {
-	const actual = await vi.importActual("./mockMovies.json");
-	return actual;
+  const actual = await vi.importActual("./mockMovies.json");
+  return actual;
 });
 
 describe("MovieList", () => {
-    beforeEach(() => {
-        render(
-            <UserProvider>
-                <BookmarkProvider>
-                    <MemoryRouter initialEntries={["/"]}>
-                        <MovieList movies={moviesData} />
-                    </MemoryRouter>
-                </BookmarkProvider>
-            </UserProvider>
-        );
-    });
+  beforeEach(() => {
+    render(
+      <UserProvider>
+        <BookmarkProvider>
+          <MemoryRouter initialEntries={["/"]}>
+            <MovieList movies={moviesData} />
+          </MemoryRouter>
+        </BookmarkProvider>
+      </UserProvider>
+    );
+  });
 
-    it("renders the Trending and Recommended sections", () => {
-        expect(screen.getByRole("heading", { name: /trending/i })).toBeInTheDocument();
-        expect(screen.getByRole("heading", { name: /recommended for you/i })).toBeInTheDocument();
-    });
+  it("renders the Trending and Recommended sections", () => {
+    expect(
+      screen.getByRole("heading", { name: /trending/i })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("heading", { name: /recommended for you/i })
+    ).toBeInTheDocument();
+  });
 
-    it("renders Recommended movie", () => {
-    
+  it("renders Recommended movie", () => {
     const recommendedMovieTitles = moviesData
-        .filter(movie => !movie.isTrending)
-        .map(movie => movie.title);
-    
-    
-    recommendedMovieTitles.forEach(movieTitle => {
-        const movieImgElement = screen.getByAltText(movieTitle);            
-        expect(movieImgElement).toBeInTheDocument();
-    });
-        
-    });
-    
+      .filter((movie) => !movie.isTrending)
+      .map((movie) => movie.title);
 
-it("renders Trending movies", () => {
-        
-        const trendingMovieImages = moviesData
-            .filter(movie => movie.isTrending)
-            .map(movie => movie.title);
-        console.log(trendingMovieImages);
-        trendingMovieImages.forEach(movie => {
-            const movieImgElement = screen.getByAltText(movie);
-            
-            expect(movieImgElement).toBeInTheDocument();
-        });
-    
+    recommendedMovieTitles.forEach((movieTitle) => {
+      const movieImgElement = screen.getByAltText(movieTitle);
+      expect(movieImgElement).toBeInTheDocument();
+    });
+  });
+
+  it("renders Trending movies", () => {
+    const trendingMovieImages = moviesData
+      .filter((movie) => movie.isTrending)
+      .map((movie) => movie.title);
+    console.log(trendingMovieImages);
+    trendingMovieImages.forEach((movie) => {
+      const movieImgElement = screen.getByAltText(movie);
+
+      expect(movieImgElement).toBeInTheDocument();
+    });
+  });
+
+  it("renders the correct number of movies in the Recommended section", () => {
+    const recommended = screen.getByTestId("recommended");
+    const movieThumbnail = within(recommended).getAllByRole("img");
+    expect(movieThumbnail).toHaveLength(6);
+  });
+
+  it("renders the correct number of movies in the Trending section", () => {
+    const treading = screen.getByTestId("trending");
+    const movieThumbnail = within(treading).getAllByRole("img");
+    expect(movieThumbnail).toHaveLength(6);
+  });
 });
-    
-
-    it("renders the correct number of movies in the Recommended section", () => {
-        const recommended = screen.getByTestId("recommended");
-        const movieThumbnail = within(recommended).getAllByRole("img");
-        expect(movieThumbnail).toHaveLength(6); 
-    });
-
-    it("renders the correct number of movies in the Trending section", () =>
-    {
-        const treading = screen.getByTestId("trending");
-        const movieThumbnail = within(treading).getAllByRole("img");
-        expect(movieThumbnail).toHaveLength(6); 
-    })
-
-    });
